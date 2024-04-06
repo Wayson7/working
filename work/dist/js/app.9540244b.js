7cee84cc8e48d5593e30adc3be01a83b296b6a21
Wayson7 <xiaosong3380@gmail.com>
2024-04-06 20:59:15 +0700

    version 1.5

---
+++ work/dist/js/app.9540244b.js
@@ -0,0 +1,89 @@
+(function(){"use strict";var e={6917:function(e,t,o){var i=o(5130),n=o(6768),r=o.p+"img/me.8faf4ecd.jpg";const l=(0,n.Lk)("div",{id:"logo"},[(0,n.Lk)("img",{id:"img",alt:"Vue logo",src:r})],-1);function a(e,t,o,i,r,a){const c=(0,n.g2)("HelloWorld");return(0,n.uX)(),(0,n.CE)(n.FK,null,[l,(0,n.bF)(c)],64)}var c=o(4232);const s=e=>((0,n.Qi)("data-v-1648ee19"),e=e(),(0,n.jt)(),e),u={id:"hello"},p=s((()=>(0,n.Lk)("div",{style:{"text-align":"left"}},[(0,n.Lk)("span",null,"日常话术")],-1))),d=["onClick","onMouseover","title"],g=["onClick"],w=s((()=>(0,n.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),v=s((()=>(0,n.Lk)("div",{style:{"text-align":"left"}},[(0,n.Lk)("span",null,"iOS话术")],-1))),m=["onClick","onMouseover","title"],h=["onClick"],k=s((()=>(0,n.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),y=s((()=>(0,n.Lk)("div",{style:{"text-align":"left"}},[(0,n.Lk)("span",null,"Windows话术")],-1))),f=["onClick","onMouseover","title"],b=["onClick"],C=s((()=>(0,n.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),I=s((()=>(0,n.Lk)("div",{style:{"text-align":"left"}},[(0,n.Lk)("span",null,"macOS话术")],-1))),P=["onClick","onMouseover","title"],O=["onClick"],T=s((()=>(0,n.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),x=s((()=>(0,n.Lk)("div",{style:{"text-align":"left"}},[(0,n.Lk)("span",null,"Androidi话术")],-1))),L=["onClick","onMouseover","title"],S=["onClick"],E=s((()=>(0,n.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),M=s((()=>(0,n.Lk)("div",{style:{clear:"both"}},null,-1)));function F(e,t,o,r,l,a){return(0,n.uX)(),(0,n.CE)("div",u,[(0,n.Lk)("h2",{id:"notice",style:(0,c.Tr)({opacity:l.noticeOpacity})},null,4),(0,n.Lk)("ul",null,[p,((0,n.uX)(!0),(0,n.CE)(n.FK,null,(0,n.pI)(e.items,((e,o)=>((0,n.uX)(),(0,n.CE)("li",{class:"box",key:o,onClick:t=>a.copyImageOrTextToClipboard(e),onMouseover:t=>a.showPreview(e.img),onMouseleave:t[1]||(t[1]=(...e)=>a.hidePreview&&a.hidePreview(...e)),title:e.content},[(0,n.Lk)("span",null,(0,c.v_)(e.title),1),l.showPreviewFlag?((0,n.uX)(),(0,n.CE)("div",{key:0,class:"preview",style:(0,c.Tr)({backgroundImage:`url(${l.previewImg})`}),onClick:(0,i.D$)((t=>a.copyImageOrTextToClipboard(e)),["stop"]),onMouseenter:t[0]||(t[0]=(...e)=>a.hidePreview&&a.hidePreview(...e))},null,44,g)):(0,n.Q3)("",!0)],40,d)))),128)),w]),(0,n.Lk)("ul",null,[v,((0,n.uX)(!0),(0,n.CE)(n.FK,null,(0,n.pI)(e.ios,((e,o)=>((0,n.uX)(),(0,n.CE)("li",{class:"box",key:o,onClick:t=>a.copyImageOrTextToClipboard(e),onMouseover:t=>a.showPreview(e.img),onMouseleave:t[3]||(t[3]=(...e)=>a.hidePreview&&a.hidePreview(...e)),title:e.content},[(0,n.Lk)("span",null,(0,c.v_)(e.title),1),l.showPreviewFlag?((0,n.uX)(),(0,n.CE)("div",{key:0,class:"preview",style:(0,c.Tr)({backgroundImage:`url(${l.previewImg})`}),onClick:(0,i.D$)((t=>a.copyImageOrTextToClipboard(e)),["stop"]),onMouseenter:t[2]||(t[2]=(...e)=>a.hidePreview&&a.hidePreview(...e))},null,44,h)):(0,n.Q3)("",!0)],40,m)))),128)),k]),(0,n.Lk)("ul",null,[y,((0,n.uX)(!0),(0,n.CE)(n.FK,null,(0,n.pI)(e.windows,((e,o)=>((0,n.uX)(),(0,n.CE)("li",{class:"box",key:o,onClick:t=>a.copyImageOrTextToClipboard(e),onMouseover:t=>a.showPreview(e.img),onMouseleave:t[5]||(t[5]=(...e)=>a.hidePreview&&a.hidePreview(...e)),title:e.content},[(0,n.Lk)("span",null,(0,c.v_)(e.title),1),l.showPreviewFlag?((0,n.uX)(),(0,n.CE)("div",{key:0,class:"preview",style:(0,c.Tr)({backgroundImage:`url(${l.previewImg})`}),onClick:(0,i.D$)((t=>a.copyImageOrTextToClipboard(e)),["stop"]),onMouseenter:t[4]||(t[4]=(...e)=>a.hidePreview&&a.hidePreview(...e))},null,44,b)):(0,n.Q3)("",!0)],40,f)))),128)),C]),(0,n.Lk)("ul",null,[I,((0,n.uX)(!0),(0,n.CE)(n.FK,null,(0,n.pI)(e.mac,((e,o)=>((0,n.uX)(),(0,n.CE)("li",{class:"box",key:o,onClick:t=>a.copyImageOrTextToClipboard(e),onMouseover:t=>a.showPreview(e.img),onMouseleave:t[7]||(t[7]=(...e)=>a.hidePreview&&a.hidePreview(...e)),title:e.content},[(0,n.Lk)("span",null,(0,c.v_)(e.title),1),l.showPreviewFlag?((0,n.uX)(),(0,n.CE)("div",{key:0,class:"preview",style:(0,c.Tr)({backgroundImage:`url(${l.previewImg})`}),onClick:(0,i.D$)((t=>a.copyImageOrTextToClipboard(e)),["stop"]),onMouseenter:t[6]||(t[6]=(...e)=>a.hidePreview&&a.hidePreview(...e))},null,44,O)):(0,n.Q3)("",!0)],40,P)))),128)),T]),(0,n.Lk)("ul",null,[x,((0,n.uX)(!0),(0,n.CE)(n.FK,null,(0,n.pI)(e.android,((e,o)=>((0,n.uX)(),(0,n.CE)("li",{class:"box",key:o,onClick:t=>a.copyImageOrTextToClipboard(e),onMouseover:t=>a.showPreview(e.img),onMouseleave:t[9]||(t[9]=(...e)=>a.hidePreview&&a.hidePreview(...e)),title:e.content},[(0,n.Lk)("span",null,(0,c.v_)(e.title),1),l.showPreviewFlag?((0,n.uX)(),(0,n.CE)("div",{key:0,class:"preview",style:(0,c.Tr)({backgroundImage:`url(${l.previewImg})`}),onClick:(0,i.D$)((t=>a.copyImageOrTextToClipboard(e)),["stop"]),onMouseenter:t[8]||(t[8]=(...e)=>a.hidePreview&&a.hidePreview(...e))},null,44,S)):(0,n.Q3)("",!0)],40,L)))),128)),E]),M])}o(4603),o(7566),o(8721);var X=o(782),$={name:"HelloWorld",data(){return{showPreviewFlag:!1,previewImg:"",noticeOpacity:0,previewLoaded:!1}},computed:{...(0,X.L8)(["items","ios","windows","mac","android"])},methods:{async copyImageOrTextToClipboard(e){try{if(!navigator.clipboard)return void console.error("Clipboard API is not supported.");if(""===e.content){if(""===e.img)return void console.error("无可复制内容");const t=await fetch(e.img).then((e=>e.blob())),o=new ClipboardItem({"image/png":t});await navigator.clipboard.write([o]),console.log("图片已成功复制到剪贴板")}else await navigator.clipboard.writeText(e.content),console.log("文字内容已成功复制到剪贴板");if(this.noticeOpacity=1,document.getElementById("notice").innerText="复制成功",setTimeout((()=>{this.noticeOpacity=0,document.getElementById("notice").innerText=""}),1e3),!navigator.clipboard)return console.error("Clipboard API is not supported."),document.execCommand("copy"),void console.log("文本已成功复制到剪贴板");if(""===e.content){if(""===e.img)return void console.error("无可复制内容");const t=await fetch(e.img).then((e=>e.blob())),o=new ClipboardItem({"image/png":t});await navigator.clipboard.write([o]),console.log("图片已成功复制到剪贴板")}else navigator.clipboard.writeText(e.content).then((()=>{console.log("文字内容已成功复制到剪贴板")})).catch((e=>{console.error("复制失败: ",e)}));this.noticeOpacity=1,document.getElementById("notice").innerText="复制成功",setTimeout((()=>{this.noticeOpacity=0,document.getElementById("notice").innerText=""}),1e3)}catch(t){console.error("复制到剪贴板失败：",t)}},async showPreview(e){if(this.previewImg===e&&this.previewLoaded)return void(this.showPreviewFlag=!0);const t=await fetch(e).then((e=>e.blob()));this.previewImg=URL.createObjectURL(t),this.previewLoaded=!0,this.showPreviewFlag=!0},hidePreview(){this.showPreviewFlag=!1,this.previewImg=""}}},_=o(1241);const D=(0,_.A)($,[["render",F],["__scopeId","data-v-1648ee19"]]);var j=D,A={name:"App",components:{HelloWorld:j}};const G=(0,_.A)(A,[["render",a]]);var K=G;const Q=(0,X.y$)({state(){return{items:[{title:"官网下载",content:String.raw`请收藏以下官网:
+    https://cloudupup.net (复制去浏览器~)
+    https://47.122.18.2 (复制去浏览器~)
+    https://cloudupup14.com (复制去浏览器~)
+
+ ~新用户~：
+注册、购买套餐、下载软件、导入订阅地址。
+ ~老用户~：
+无法使用旧线路,请删除旧线路后从官网续费并导入新订阅地址。
+
+安装教程：
+https://help.cloudupup.com
+永久在线客服,保持联系,谢谢!`,img:""},{title:"GPT,iOS等账号销售",content:String.raw`这边有GPT3.5和4.0, 谷歌账号, iOS ID正在销售
+    
+    https://idinshop.com/ (复制去浏览器哦~)
+
+您可以进入链接查看更详细产品介绍`,img:""},{title:"年卡七折/优惠",content:String.raw`年卡:190元--月卡22元*12=264元
+年卡是月卡的7折，可以冲
+
+我们是专线独享套餐,比其他产品的公网服务成本要高7-8倍
+稳定性也是最好的，可以放心使用
+同类产品价格基本都是翻倍，主打品质，感谢您的支持~
+大流量和超大流量套餐 折扣更是划算~`,img:""},{title:"邀请返利",content:String.raw`您可以登陆官网=>我的账号=>邀请返利
+把邀请返利有个'链接地址', 发给朋友注册
+
+每次朋友购买都会有20%返利到您的账户,
+满300可以提现也可以发工单,
+或者转成余额,抵扣套餐费用哦~
+
+感谢您的支持和关注~~我们会继续加油的
+提供更好品质的服务！！`,img:""},{title:"注销原因",content:String.raw`为了用户的账户安全, 我们会在套餐过期一个月后会自动注销, 可以保障用户的个人信息安全, 您可以用原有的账号重新注册并购买即可~`,img:""}],ios:[{title:"iOS更新订阅 (文)",content:String.raw`1. 关闭Shadowrocket连接。
+2. 在小火箭首页, 从左往右滑删除旧线路信息
+3. 使用苹果自带浏览器登录以下官网:
+    https://cloudupup.net (复制去浏览器~)
+    https://47.122.18.2 (复制去浏览器~)
+    https://cloudupup14.com (复制去浏览器~)
+（替换原有的订阅地址）
+4. 在官网底部找到订阅地址,选择Shadowrocket,然后点击'一键导入'
+    (也可以点击美区ID下方的'更新订阅'一键导入)
+5. 返回Shadowrocket,线路已经刷新
+6. 选择任何国家的节点,在Shadowrocket中将全局路由改为'代理'模式,连接即可使用
+
+!!请确保在软件内检查是否过期,并确认是否有足够的流量!!`,img:""},{title:"iOS更新订阅 (图)",content:"",img:o(2035)},{title:"iOS安装教程 (文)",content:String.raw`Apple ID切换教程:
+1、登陆官网：
+    https://cloudupup.net
+    https://47.122.18.2
+    https://cloudupup14.com
+2、 iOS美区共享id,登陆美区ID
+    （付费用户才能看到共享id）
+3、如果登陆没有跳转美区，从下上滑屏幕，然后在进程中关闭苹果商店，重新打开
+4、登录时，如弹出安全验证，选'其他选择'=>'不升级'
+成功进入后:
+5、搜索Shadowrocket（需要pin/推特/ins等，可一块搜索下载）
+6、下载后请退出共享id（共享id会在20分钟后自动修改密码）
+4、苹果自带浏览器登陆'我们的官网'点击=>ios订阅更新（shadowrocket）
+                    
+ios详细教程地址：
+https://help.cloudupup.com/ios/Shadowrocket.html`,img:""},{title:"iOS切换美区ID (图)",content:"",img:o(1732)}],windows:[{title:"V2ray更新Cloudupup",content:String.raw`v2ray如无法使用，替换cloudupup(红色图标)
+新客户端更简洁的页面, 更方便的操作
+自动化更新, 内含永久客服, 官网账号密码登录即可。
+    https://cloudupup.net (复制去浏览器~)
+    https://47.122.18.2 (复制去浏览器~)
+    https://cloudupup14.com (复制去浏览器~)
+
+windows的cloudup详细教程：
+https://help.cloudupup.com/win/upupforwin.html`,img:""},{title:"Cloudupup常见问题",content:String.raw`常见问题: win电脑还无法使用cloudup？
+1、必须卸载360,腾讯管家,火绒,鲁大师,反诈app等安全软件, 且关闭防火墙
+2、确认电脑的时间是北京时间，不能差1秒
+3、换个浏览器打开网站, 或尝试无痕模式
+4、不同时开多个代理, 会起冲突, 关闭软件后=>重启电脑=>右键点击图标=>管理员模式运行
+5、更换新的网络环境, 也可以用手机开热点试试,
+然后重启电脑再试试`,img:""}],mac:[{title:"Mac打开终端方式",content:String.raw`1、您可以选择以下三种操作打开终端:
+(1) 打开启动台=>其他=>终端
+(2) 打开访达=>直接搜索'终端'
+(3) 点击桌面右上角的发动机,搜索'终端'`,img:""}],android:[{title:"安卓海外应用",content:String.raw`可以访问链接进行下载海外应用
+(复制去浏览器~)
+    https://alist.wdnmd.me/dropbox
+需要先启动代理才能下载哦~`,img:""},{title:"谷歌商店无法使用",content:String.raw`GOOGLEPLAY商店无法下载：
+1. 清除Play商店和谷歌三件套应用的数据和缓存后:
+    (1)点击'个人设置'=>点击'应用'，找到Google Play商店, Google服务框架等应用。
+    (2)按'清除缓存数据'，然后在翻墙软件打开'全局模式'重新尝试。
+
+2. 新机内置Play为原厂预置中国版框架，需要去'apkpure'或者'apkmirror'下载新谷歌三件套来框架来覆盖安装。
+下载地址：https://github.com/hideuvpn/android-google-play-store
+
+3. 麻烦提供您所用的线路名称，我们检查一下，您可以先换其他线路测试，可以试试美国等其他线路。`,img:""},{title:"安卓海外应用",content:String.raw`可以访问链接进行下载海外应用
+https://alist.wdnmd.me/dropbox(复制去浏览器)
+需要先启动代理才能下载哦~`,img:""}]}},getters:{items(e){return e.items},ios(e){return e.ios},windows(e){return e.windows},mac(e){return e.mac},android(e){return e.android}},mutations:{preloadImages(e){const t=[...e.items,...e.ios,...e.windows,...e.mac,...e.android];t.forEach((e=>{if(""!==e.img){const t=new Image;t.src=e.img,t.onload=()=>{localStorage.setItem(e.img,t.src)}}}))}},actions:{preloadImages({commit:e}){e("preloadImages")}}});var B=Q;const W=(0,i.Ef)(K);W.use(B),W.mount("#app")},1732:function(e,t,o){e.exports=o.p+"img/ios_shadowrocket.cb28c96b.png"},2035:function(e,t,o){e.exports=o.p+"img/ios_update.821a26bc.png"}},t={};function o(i){var n=t[i];if(void 0!==n)return n.exports;var r=t[i]={exports:{}};return e[i].call(r.exports,r,r.exports,o),r.exports}o.m=e,function(){var e=[];o.O=function(t,i,n,r){if(!i){var l=1/0;for(u=0;u<e.length;u++){i=e[u][0],n=e[u][1],r=e[u][2];for(var a=!0,c=0;c<i.length;c++)(!1&r||l>=r)&&Object.keys(o.O).every((function(e){return o.O[e](i[c])}))?i.splice(c--,1):(a=!1,r<l&&(l=r));if(a){e.splice(u--,1);var s=n();void 0!==s&&(t=s)}}return t}r=r||0;for(var u=e.length;u>0&&e[u-1][2]>r;u--)e[u]=e[u-1];e[u]=[i,n,r]}}(),function(){o.n=function(e){var t=e&&e.__esModule?function(){return e["default"]}:function(){return e};return o.d(t,{a:t}),t}}(),function(){o.d=function(e,t){for(var i in t)o.o(t,i)&&!o.o(e,i)&&Object.defineProperty(e,i,{enumerable:!0,get:t[i]})}}(),function(){o.g=function(){if("object"===typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(e){if("object"===typeof window)return window}}()}(),function(){o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)}}(),function(){o.p=""}(),function(){var e={524:0};o.O.j=function(t){return 0===e[t]};var t=function(t,i){var n,r,l=i[0],a=i[1],c=i[2],s=0;if(l.some((function(t){return 0!==e[t]}))){for(n in a)o.o(a,n)&&(o.m[n]=a[n]);if(c)var u=c(o)}for(t&&t(i);s<l.length;s++)r=l[s],o.o(e,r)&&e[r]&&e[r][0](),e[r]=0;return o.O(u)},i=self["webpackChunkwork"]=self["webpackChunkwork"]||[];i.forEach(t.bind(null,0)),i.push=t.bind(null,i.push.bind(i))}();var i=o.O(void 0,[504],(function(){return o(6917)}));i=o.O(i)})();
+//# sourceMappingURL=app.9540244b.js.map
