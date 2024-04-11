c8bd25d1d98826d5d911ba1c1c60d897282526b1
Wayson7 <xiaosong3380@gmail.com>
2024-04-11 12:10:19 +0700

    update

---
+++ dist/js/app.4e4ec4e5.js
@@ -0,0 +1,174 @@
+(function(){"use strict";var t={8604:function(t,o,n){var i=n(5130),e=n(6768),l=n.p+"img/me.8faf4ecd.jpg";const c=(0,e.Lk)("div",{id:"logo"},[(0,e.Lk)("img",{id:"img",alt:"Vue logo",src:l})],-1);function p(t,o,n,i,l,p){const r=(0,e.g2)("HelloWorld");return(0,e.uX)(),(0,e.CE)(e.FK,null,[c,(0,e.bF)(r)],64)}var r=n(4232);const a=t=>((0,e.Qi)("data-v-1c1c0ada"),t=t(),(0,e.jt)(),t),u={id:"hello"},s=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"日常话术")],-1))),d=["onClick","title"],m=["onClick"],g=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),h=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"软件使用话术")],-1))),k=["onClick","title"],w=["onClick"],f=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),y=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"iOS&iPad话术")],-1))),C=["onClick","title"],v=["onClick"],b=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),x=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"Windows话术")],-1))),L=["onClick","title"],T=["onClick"],I=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),S=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"macOS话术")],-1))),O=["onClick","title"],P=["onClick"],E=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),_=a((()=>(0,e.Lk)("div",{style:{"text-align":"left"}},[(0,e.Lk)("span",null,"Android话术")],-1))),D=["onClick","title"],X=["onClick"],A=a((()=>(0,e.Lk)("li",{style:{"list-style":"none",clear:"both"}},null,-1))),G=a((()=>(0,e.Lk)("div",{style:{clear:"both"}},null,-1))),F=a((()=>(0,e.Lk)("div",{style:{height:"300px"}},null,-1)));function j(t,o,n,l,c,p){return(0,e.uX)(),(0,e.CE)("div",u,[(0,e.Lk)("h2",{id:"notice",style:(0,r.Tr)({opacity:c.noticeOpacity})},null,4),(0,e.Lk)("ul",null,[s,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.items,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,m)],8,d)))),128)),g]),(0,e.Lk)("ul",null,[h,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.app,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,w)],8,k)))),128)),f]),(0,e.Lk)("ul",null,[y,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.ios,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,v)],8,C)))),128)),b]),(0,e.Lk)("ul",null,[x,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.windows,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,T)],8,L)))),128)),I]),(0,e.Lk)("ul",null,[S,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.mac,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,P)],8,O)))),128)),E]),(0,e.Lk)("ul",null,[_,((0,e.uX)(!0),(0,e.CE)(e.FK,null,(0,e.pI)(t.android,((t,o)=>((0,e.uX)(),(0,e.CE)("li",{class:"box",key:o,onClick:o=>p.copyImageOrTextToClipboard(t),title:t.content},[(0,e.Lk)("span",null,(0,r.v_)(t.title),1),(0,e.Lk)("div",{class:"preview",onClick:(0,i.D$)((o=>p.copyImageOrTextToClipboard(t)),["stop"])},null,8,X)],8,D)))),128)),A]),G,F])}var $=n(782),K={name:"HelloWorld",data(){return{noticeOpacity:0}},computed:{...(0,$.L8)(["items","app","ios","windows","mac","android"])},methods:{async copyToClipboard(t){try{if(!navigator.clipboard)return void console.error("Clipboard API is not supported.");await navigator.clipboard.writeText(t),console.log("内容已成功复制到剪贴板")}catch(o){console.error("复制到剪贴板失败：",o)}},async copyImageOrTextToClipboard(t){try{let o="";if(""===t.content){if(""===t.img)return void console.error("无可复制内容");const n=await fetch(t.img).then((t=>t.blob())),i=new ClipboardItem({"image/png":n});await navigator.clipboard.write([i]),o="图片"}else await this.copyToClipboard(t.content),o="文字";this.noticeOpacity=1,document.getElementById("notice").innerText=`${o}复制成功`,setTimeout((()=>{this.noticeOpacity=0,document.getElementById("notice").innerText=""}),1e3)}catch(o){console.error("复制到剪贴板失败：",o)}}}},V=n(1241);const W=(0,V.A)(K,[["render",j],["__scopeId","data-v-1c1c0ada"]]);var H=W,M={name:"App",components:{HelloWorld:H}};const Q=(0,V.A)(M,[["render",p]]);var B=Q;const N=(0,$.y$)({state(){return{items:[{title:"官网下载",content:String.raw`请登陆并收藏以下官网:
+    https://cloudupup.net (复制去浏览器~)
+    https://47.122.18.2 (复制去浏览器~)
+    https://cloudupup15.com (复制去浏览器~)
+(如无法使用可以将当前数字往上加1,
+例如:https://cloudupup16.com)
+
+ ~新用户~ios
+注册、购买套餐、下载软件、导入订阅地址。
+ ~老用户~：
+ 官网续费、更新订阅/线路、下载新客户端
+
+安装教程：
+https://help.cloudupup.com
+永久在线客服,保持联系,谢谢!`,img:""},{title:"描述详细些",content:String.raw`您好, 详细描述一下您的问题哦~这边方便帮您排查一下~最好是可以直接提供截图或视频~`,img:""},{title:"GPT,iOS等账号销售",content:String.raw`这边有GPT3.5和4.0, 谷歌账号, iOS ID正在销售
+    
+    https://idinshop.com/ (复制去浏览器哦~)
+
+您可以进入链接查看更详细产品介绍`,img:""},{title:"年卡七折/优惠",content:String.raw`年卡:190元--月卡22元*12=264元
+年卡是月卡的7折，可以冲
+
+我们是专线独享套餐,比其他产品的公网服务成本要高7-8倍
+稳定性也是最好的，可以放心使用
+同类产品价格基本都是翻倍，主打品质，感谢您的支持~
+大流量和超大流量套餐 折扣更是划算~`,img:""},{title:"邀请返利",content:String.raw`您可以登陆官网=>我的账号=>邀请返利
+把邀请返利有个'链接地址', 发给朋友注册
+
+每次朋友购买都会有20%返利到您的账户,
+满300可以提现也可以发工单,
+或者转成余额,抵扣套餐费用哦~
+
+感谢您的支持和关注~~我们会继续加油的
+提供更好品质的服务！！`,img:""},{title:"注销原因",content:String.raw`为了用户的账户安全, 我们会在套餐过期一个月后会自动注销, 可以保障用户的个人信息安全, 您可以用原有的账号重新注册并购买即可~`,img:""},{title:"开发票",content:String.raw`因为这边是海外公司, 所以无法开大陆发票, 但这边可以给您开invoice, 如果您需要的话, 写下您的公司全称和收款邮箱以及账号, 这边记录一下, 24小时内给您开, 请您耐心等待~`,img:""},{title:"微信满了",content:String.raw`抱歉,微信满 https://47.122.18.2 官网右下角联系我们`,img:""},{title:"toDesk远程",content:String.raw`您这边方便远程吗?
+我可以给您远程操作一下
+下载toDesk, 然后发链接码给我
+https://www.todesk.com/download.html`,img:""}],app:[{title:"TikTok无法使用",content:String.raw`解决办法的操作步骤如下：
+(必要条件: 可以正常访问 www.tiktok.com)
+第一步: 拔手机卡
+第二步: 由于tiktok识别到国内的sim卡后，会屏蔽。 
+设置语言:
+    将手机的语言切换成 非简体中文 即可。                    
+完成上述步骤后即可正常使用tik tok了。
+            
+注意：每次打开tik tok都需要拔掉手机卡，打开后可以正常插卡使用
+(APP版tiktok非常的严格, 网页可以正常访问的话, 网络代理是没问题的, 
+如果该方法无法使用, 您需要自行百度搜索一下其他方法哦~)`,img:""},{title:"GPT使用注意事项",content:String.raw`因为GPT的政策变更, 对大陆用户非常的严格
+封号原因主要分为账号安全和恶意行为：
+1. 账号使用的邮箱权重过低
+2. IP所在地为香港等亚洲地区（印度等地目前没太大问题）
+3. 大量使用中文进行对话（不一定立刻封号，但风险较高）
+4. 部分用户利用ChatGPT平台发布违规、虚假信息
+5. 频繁的更换IP地址, 多次异地登陆GPT
+
+避免被封号的几点建议：
+不要使用亚洲代理服务器，最好是美国服务器
+不要使用不稳定的服务器，比如过度共享的IP
+不要使用权重过低的邮箱账户，比如QQ邮箱、163邮箱，建议使用Outlook或Gmail
+不要在同一手机号下注册多个账号
+不要再页面使用中文`,img:""}],ios:[{title:"iOS安装教程",content:String.raw`美区ID切换教程及安装:
+1、登陆以下官网 都行 (复制去浏览器打开~)：
+    https://cloudupup.net 
+    https://47.122.18.2
+    https://cloudupup15.com
+2、主页=>找到 ios安装教程&美区共享id
+    （付费用户才能看到共享id）
+（如没有跳转美区, 手指从下往上滑动屏幕, 显示运行进程, 关闭AppStore进程后, 重新打开）
+4、登录时, 如弹出安全验证, 选'其他选择'=>'不升级'
+成功进入后:
+5、搜索shadowrocket（需要pin/推特/ins等, 可一块下载）
+6、下载后退出共享id （共享id 20分钟会自动修改密码）
+4、官网主页最低下找到订阅===> shadowrocket, 点击一键导入, 连接就行了
+
+ios教程地址：
+https://help.cloudupup.com/ios/Shadowrocket.html`,img:""},{title:"(图) iOS切换美区ID",content:"",img:n(806)},{title:"iOS更新订阅",content:String.raw`1. 关闭Shadowrocket连接。
+2. 在小火箭首页, 从左往右滑删除旧线路信息
+3. 使用苹果自带浏览器登录以下官网:
+(复制去浏览器打开哦~~)
+    https://cloudupup.net 
+    https://47.122.18.2
+    https://cloudupup15.com
+（替换原有的订阅地址）
+4. 在官网底部找到订阅地址,选择Shadowrocket,然后点击'一键导入'
+    (也可以点击美区ID下方的'更新订阅'一键导入)
+5. 返回Shadowrocket,线路已经刷新
+6. 选择任何国家的节点,在Shadowrocket中将全局路由改为'代理'模式,连接即可使用
+
+!!请确保在软件内检查是否过期,并确认是否有足够的流量!!`,img:""},{title:"(图) iOS更新订阅",content:"",img:n(6161)},{title:"美区ID无法登录",content:String.raw`美区id无法登录：
+1、您需要用手机开热点 ipad链接，或者手机用数据方式链接，因为美国appstore是苹果内网，对大陆的宽带网络不友好，被中国限制，手机流量4g/5g网络可以
+2、如果是安全问题，您选择，其他选项，不升级
+3、如果id有问题，换其他id尝试即可
+(在官网重新登陆刷新一下页面就会刷新出新的共享ID)
+可以使用新的美区共享ID登陆
+要记得在20分钟后退出(每20分钟更改一次密码)`,img:""},{title:"官网共享ID位置",content:"",img:n(8324)}],windows:[{title:"(图) upup使用教程",content:"",img:n(2685)},{title:"V2ray更新upup",content:String.raw`如无法使用，使用新的客户端:cloudupup(红色图标)
+新客户端更简洁的页面, 更方便的操作
+自动化更新, 内含永久客服, 官网账号密码登录即可。
+
+前往以下官网下载安装即可立刻使用:
+(复制去浏览器打开哦~)
+    https://cloudupup.net
+    https://47.122.18.2
+    https://cloudupup15.com
+
+windows的cloudup详细教程：
+https://help.cloudupup.com/win/upupforwin.html
+安装选择所有用户可用! 安装到默认地址，不要改！`,img:""},{title:"upup常见问题",content:String.raw`常见问题: win电脑还无法使用cloudup？
+1、必须卸载360,腾讯管家,火绒,鲁大师,反诈app等安全软件, 且关闭防火墙
+2、确认电脑的时间是北京时间，不能差1秒
+3、换个浏览器打开网站, 或尝试无痕模式
+4、不同时开多个代理, 会起冲突, 关闭软件后=>重启电脑=>右键点击图标=>管理员模式运行
+5、更换新的网络环境, 也可以用手机开热点试试,
+然后重启电脑再试试`,img:""},{title:'upup"连接"无反应',content:String.raw`尝试一下以下办法:
+1、关闭并卸载所有安全软件
+2、尝试进入设置关闭防火墙
+3、尝试重装软件后重启电脑
+4、打开任务管理器并结束cloudupup进程
+    在状态栏右键cloudupup小图标并选'重启并清除配置'
+5、尝试把软件安装在C盘后重启
+
+如果还是不行, 建议更换clash
+
+windows的clash详细教程：
+https://help.cloudupup.com/win/clash.html`,img:""},{title:"Clash更新教程",content:String.raw`Clash教程:
+1、进入以下官网都行：
+(复制去浏览器打开~)
+    https://cloudupup.net
+    https://47.122.18.2
+    https://cloudupup15.com
+2、登陆个人账号 -> 官网首页右下边 -> 点击 Clash订阅 -> 一键导入`,img:""},{title:"(图) Clash更新教程",content:"",img:n(1682)},{title:"(图) Clash使用教程",content:"",img:n(2634)}],mac:[{title:"(图) upup使用教程",content:"",img:n(2685)},{title:"Mac安装使用upup",content:String.raw`用cloudup客户端（自动化客户端）永久在线客服。
+可收藏永久域名(复制去浏览器打开):
+https://cloudupup.net
+http://47.122.18.2
+http://cloudupup15.com
+！！ins/pin/领英/X等社交软件，右上角设置全局模式！
+                
+https://help.cloudupup.com/mac/upupformac.html
+cloudupup 电脑macbook操作说明`,img:""},{title:"mac安装使用clash",content:String.raw`mac电脑安装clash教程:
+1、登陆官网后往下拉
+(复制去浏览器打开~)
+https://cloudupup.net
+http://47.122.18.2
+http://cloudupup15.com
+可以看到mac的clash图文安装使用教程~
+
+2、下载完成后点击clash订阅 => 一键导入即可哦~
+3、右键clash后, 出站规则改为 全局模式 以及 打开系统代理模式
+
+mac的clash详细教程：
+https://help.cloudupup.com/mac/clash.html`,img:""},{title:"(图) mac更新导入clash",content:"",img:n(9457)},{title:"(图) mac允许未知来源",content:"",img:n(3101)},{title:"Mac打开终端方式",content:String.raw`1、您可以选择以下三种操作打开终端:
+(1) 打开启动台=>其他=>终端
+(2) 打开访达=>直接搜索'终端'
+(3) 点击桌面右上角的发动机,搜索'终端'`,img:""}],android:[{title:"Android安装下载",content:String.raw`安卓下载教程
+
+`},{title:"(图) Android使用教程",content:"",img:n(8601)},{title:"Android无法使用",content:String.raw`安卓无法使用：
+1、多VP冲突，会造成权限被抢占VPN无法使用
+解决：删除所有VPN后，重启手机重装VP
+下载地址：https://cloudupup.net
+2、套餐过期或订阅过期:
+    解决：登陆官网后查看套餐或者流量是否过期，购买套餐，然后复制最新订阅，黏贴到软件里的url地址中，再更新订阅
+3、切换4g或者wifi试试，可能是路由器或者本地宽带有一些限制，或者第三方代理
+4、设备时间设置北京时间自动同步即可
+5、卸载反诈app
+6、重新安装cloudupup`,img:""},{title:"Android海外应用",content:String.raw`可以访问链接进行下载海外应用
+(复制去浏览器~)
+    https://alist.wdnmd.me/dropbox
+需要先启动代理才能下载哦~`,img:""},{title:"谷歌商店无法使用",content:String.raw`GOOGLEPLAY商店无法下载：
+1. 清除Play商店和谷歌三件套应用的数据和缓存后:
+    (1)点击'个人设置'=>点击'应用'，找到Google Play商店, Google服务框架等应用。
+    (2)按'清除缓存数据'，然后在翻墙软件打开'全局模式'重新尝试。
+
+2. 新机内置Play为原厂预置中国版框架，需要去'apkpure'或者'apkmirror'下载新谷歌三件套来框架来覆盖安装。
+下载地址：https://github.com/hideuvpn/android-google-play-store
+
+3. 麻烦提供您所用的线路名称，我们检查一下，您可以先换其他线路测试，可以试试美国等其他线路。`,img:""}]}},getters:{items(t){return t.items},ios(t){return t.ios},windows(t){return t.windows},mac(t){return t.mac},android(t){return t.android},app(t){return t.app}},mutations:{preloadImages(t){const o=[...t.items,...t.app,...t.ios,...t.windows,...t.mac,...t.android];o.forEach((t=>{if(""!==t.img){const o=new Image;o.src=t.img,o.onload=()=>{localStorage.setItem(t.img,o.src)}}}))}},actions:{preloadImages({commit:t}){t("preloadImages")}}});var Y=N;const q=(0,i.Ef)(B);q.use(Y),q.mount("#app")},8601:function(t,o,n){t.exports=n.p+"img/android_cloudupup.8f05dd73.png"},2685:function(t,o,n){t.exports=n.p+"img/cloudupup.e2acdee2.png"},806:function(t,o,n){t.exports=n.p+"img/ios_shadowrocket.b8af7138.png"},8324:function(t,o,n){t.exports=n.p+"img/ios_storeid.b78d8c54.png"},6161:function(t,o,n){t.exports=n.p+"img/ios_update.821a26bc.png"},9457:function(t,o,n){t.exports=n.p+"img/mac_clash.24d8a62f.png"},3101:function(t,o,n){t.exports=n.p+"img/mac_cloudupup.6cef58f5.png"},2634:function(t,o,n){t.exports=n.p+"img/win_clash.9395c359.png"},1682:function(t,o,n){t.exports=n.p+"img/win_clash2.eec24ab3.png"}},o={};function n(i){var e=o[i];if(void 0!==e)return e.exports;var l=o[i]={exports:{}};return t[i].call(l.exports,l,l.exports,n),l.exports}n.m=t,function(){var t=[];n.O=function(o,i,e,l){if(!i){var c=1/0;for(u=0;u<t.length;u++){i=t[u][0],e=t[u][1],l=t[u][2];for(var p=!0,r=0;r<i.length;r++)(!1&l||c>=l)&&Object.keys(n.O).every((function(t){return n.O[t](i[r])}))?i.splice(r--,1):(p=!1,l<c&&(c=l));if(p){t.splice(u--,1);var a=e();void 0!==a&&(o=a)}}return o}l=l||0;for(var u=t.length;u>0&&t[u-1][2]>l;u--)t[u]=t[u-1];t[u]=[i,e,l]}}(),function(){n.n=function(t){var o=t&&t.__esModule?function(){return t["default"]}:function(){return t};return n.d(o,{a:o}),o}}(),function(){n.d=function(t,o){for(var i in o)n.o(o,i)&&!n.o(t,i)&&Object.defineProperty(t,i,{enumerable:!0,get:o[i]})}}(),function(){n.g=function(){if("object"===typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"===typeof window)return window}}()}(),function(){n.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)}}(),function(){n.p=""}(),function(){var t={524:0};n.O.j=function(o){return 0===t[o]};var o=function(o,i){var e,l,c=i[0],p=i[1],r=i[2],a=0;if(c.some((function(o){return 0!==t[o]}))){for(e in p)n.o(p,e)&&(n.m[e]=p[e]);if(r)var u=r(n)}for(o&&o(i);a<c.length;a++)l=c[a],n.o(t,l)&&t[l]&&t[l][0](),t[l]=0;return n.O(u)},i=self["webpackChunkwork"]=self["webpackChunkwork"]||[];i.forEach(o.bind(null,0)),i.push=o.bind(null,i.push.bind(i))}();var i=n.O(void 0,[504],(function(){return n(8604)}));i=n.O(i)})();
+//# sourceMappingURL=app.4e4ec4e5.js.map
